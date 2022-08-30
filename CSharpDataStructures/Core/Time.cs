22924d0eb7007379f88d809380592f9b08c27209
alimertcetin <mrtmert54@gmail.com>
2022-08-31 01:11:41 +0300

    Create Time.cs

---
+++ CSharpDataStructures/Core/Time.cs
@@ -0,0 +1,40 @@
+﻿namespace XIV.Core
+{
+    using System.Diagnostics;
+
+    public static class Time
+    {
+        /// <summary>
+        /// Time passed between frames in seconds
+        /// </summary>
+        public static float DeltaTime => deltaTime;
+
+        /// <summary>
+        /// Time passed since the application start
+        /// </summary>
+        public static float TimeSinceStartup => timeSinceStartup;
+
+
+        static float deltaTime;
+        static float timeSinceStartup;
+        static Stopwatch stopwatch = new Stopwatch();
+
+        internal static void Start()
+        {
+            stopwatch.Start();
+        }
+
+        internal static void Update()
+        {
+            deltaTime = stopwatch.ElapsedMilliseconds;
+            deltaTime /= 1000f;
+            stopwatch.Restart();
+        }
+
+        internal static void Stop()
+        {
+            stopwatch.Stop();
+        }
+
+    }
+}
